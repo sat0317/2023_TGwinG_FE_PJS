674a11bb4560346413f98bc36001aba1efe8c71c
sat0317 <sat0317@naver.com>
2023-04-03 23:16:46 +0900

    과제 제출 (예외 발견하여 수정)

--- 3week_assignment.py
+++ 3week_assignment.py
@@ -2,11 +2,11 @@
 def grade(score):
     if 90<=score<=100:
         return "A"
-    elif 80<=score:
+    elif 80<=score<90:
         return "B"
-    elif 70<=score:
+    elif 70<=score<80:
         return "C"
-    elif 60<=score:
+    elif 60<=score<70:
         return "D"
     else:
         return "F"
